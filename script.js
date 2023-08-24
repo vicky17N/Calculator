74c3c0988acd50eb44c1f871023909fc50e497d6
VIGNESHWARAN N <125790147+vicky17N@users.noreply.github.com>
2023-08-24 18:09:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+ let input = document.getElementById('inputbox');
+ let btn = document.querySelectorAll('button')
+
+ let string = "";
+ let arr = Array.from(btn);
+ arr.forEach(btn =>{
+
+    btn.addEventListener('click',
+    (e) =>{
+        if(e.target.innerHTML == '='){
+            string = eval(string);
+            input.value = string;
+        }
+        else if (e.target.innerHTML == 'AC'){
+            string.valueOf = "";
+            input.string = string;
+        }
+        else if (e.target.innerHTML == 'DEL'){
+            string = string.substring(0,string.length -1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+ })
