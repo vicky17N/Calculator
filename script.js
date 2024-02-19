f478421c21ec88ac4f8f94d21250a2575715f764
VIGNESHWARAN N <125790147+vicky17N@users.noreply.github.com>
2024-02-19 11:53:17 +0530

    Update script.js
    change full code

--- script.js
+++ script.js
@@ -1,27 +1,22 @@
- let input = document.getElementById('inputbox');
- let btn = document.querySelectorAll('button')
+let input = document.getElementById('inputbox');
+let btns = document.querySelectorAll('button');
 
- let string = "";
- let arr = Array.from(btn);
- arr.forEach(btn =>{
-
-    btn.addEventListener('click',
-    (e) =>{
-        if(e.target.innerHTML == '='){
+let string = "";
+let arr = Array.from(btns);
+arr.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
             string = eval(string);
             input.value = string;
-        }
-        elif (e.target.innerHTML == 'AC'){
-            string.valueOf = "";
-            input.string = string;
-        }
-        elif (e.target.innerHTML == 'DEL'){
-            string = string.substring(0,string.length -1);
+        } else if (e.target.innerHTML == 'AC') {
+            string = "";
+            input.value = "";
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
             input.value = string;
-        }
-        else {
+        } else {
             string += e.target.innerHTML;
             input.value = string;
         }
-    })
- })
+    });
+});
